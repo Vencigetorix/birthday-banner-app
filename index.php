8c9e7cde9a5cc48b01f6ade3ce6e9aadcdb5cf9c
Abdiel Vladimir Romero Velázquez <vla1305@hotmail.com>
2021-10-05 14:54:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php include_once("index.html"); ?>
+<?php include_once("index.html");
+$host="us-cdbr-east-04.cleardb.com";
+$user="bba7ba2004ed75";
+$pass="8fcc9cd0";
+$db="heroku_6b4cf65f4c17d8e";
+$coneccion=mysqli_connect($host,$user,$pass,$db) or die("Error conección");
+$query="CREATE TABLE USER(id int AUTO_INCREMENT, nombre varchar(70) not null, fechanac date not null, PRIMARY KEY (id))";
+$res=mysqli_query($coneccion,$query) or die (" Error de consulta");
+?>
