786e58c347c08bb592872ac1452d9eb793009cdd
Abdiel Vladimir Romero Velázquez <vla1305@hotmail.com>
2021-10-05 15:04:08 -0500

    Registro

--- base.php
+++ base.php
@@ -6,78 +6,63 @@
   $db="heroku_6b4cf65f4c17d8e";
   $coneccion=mysqli_connect($host,$user,$pass,$db) or die("Error conección");
 
-$nomb=$_POST['nomb'];
-$nomTutor=$_POST['nomTutor'];
-$fechaNiño=$_POST['fechaNiño'];
-$sexoNiño=$_POST['sexoNiño'];
-$edNiño=$_POST['edNiño'];
-$curp=$_POST['curp'];
-
-echo "Nombre: " . $nomb  . "<br>";
-echo "Tutor: " . $nomTutor. " <br>";
-echo "Fecha Alumno: " . $fechaNiño. " <br>";
-
-echo "Sexo niño: " . $sexoNiño. " <br>";
-echo "Edad del niño: " . $edNiño. " <br>";
-echo "Curp: " . $curp. " <br>";
-
-$query="SELECT COUNT(*) from alumnos";
-$res=mysqli_query($coneccion,$query) or die (" Error de consulta");
-if ($row = mysqli_fetch_row($res)) {
-  $count = trim($row[0]);
-  }
-echo "a".$count;
-if($count < 50){
-  $query="INSERT INTO alumnos (Nom_Alumno, Nom_Tutor, Fecha_nacimiento, Sexo, Edad, Curp) VALUES ('$nomb','$nomTutor', '$fechaNiño', '$sexoNiño', '$edNiño', '$curp')";
-
-  $res=mysqli_query($coneccion,$query) or die (" Error de consulta");
-  $query="SELECT @@identity AS id";
-  $res=mysqli_query($coneccion,$query) or die (" Error de consulta id");
+  $nomb=$_POST['nomb'];
+  $fecha=$_POST['fecha'];
+  
+  echo "Nombre: " . $nomb  . "<br>";
+  echo "Fecha" . $fecha. " <br>";
+  
+  $query="SELECT COUNT(*) from user";
+  $res=mysqli_query($coneccion,$query) or die (" Error de consulta show");
   if ($row = mysqli_fetch_row($res)) {
-    $id = trim($row[0]);
+    $count = trim($row[0]);
     }
-  //echo "aaaa".$id."<br>";
-
-  $query="select * from alumnos";
-
-  $res=mysqli_query($coneccion,$query) or die ("Error de consulta 2");
-
-
-
-  $tabla="<table>
-  <tr>
-    <th> Nom_Alumno|| </th> 
-    <th> Nom_Tutor||  </th> 
-    <th> Fecha_nacimiento||  </th>
-    <th> Sexo||  </th>
-    <th> Edad||  </th>
-    <th> Curp  </th>
-  </tr>
-  ";
-
-  while ($fila=mysqli_fetch_assoc($res))
-  {
-      $tabla=$tabla."<tr><td>".$fila['Nom_Alumno']."</td><td>".$fila['Nom_Tutor']."</td><td>".$fila['Fecha_nacimiento']."</td><td>".$fila['Sexo']."</td><td>".$fila['Edad']."</td><td>".$fila['Curp']."</td></tr>";
+  echo "a".$count;
+  if($count < 50){
+    $query="INSERT INTO user (nombre, fechanac) VALUES ('$nomb', '$fecha')";
+  
+    $res=mysqli_query($coneccion,$query) or die (" Error de consulta insert");
+    $query="SELECT @@identity AS id";
+    $res=mysqli_query($coneccion,$query) or die (" Error de consulta id");
+    if ($row = mysqli_fetch_row($res)) {
+      $id = trim($row[0]);
+      }
+    $query="select * from user";
+  
+    $res=mysqli_query($coneccion,$query) or die ("Error de consulta show table");
+  
+  
+  
+    $tabla="<table>
+    <tr>
+      <th> Nombre|| </th> 
+      <th> Fecha_nacimiento||  </th>
+    </tr>
+    ";
+  
+    while ($fila=mysqli_fetch_assoc($res))
+    {
+        $tabla=$tabla."<tr><td>".$fila['nombre']."</td><td>".$fila['fechanac']."</td></tr>";
+    }
+  
+    if ($res) {
+      $rowcount=mysqli_num_rows($res);
+      echo "The total number of rows are: ".$rowcount."</td></tr>";
+      if(intdiv($id, 10) == 0){
+        echo "Su matricula es 202100".$id."</td></tr>";
+      }if(intdiv($id, 10) < 10){
+        echo "Su matricula es 20210".$id."</td></tr>";
+      }else{
+        echo "Su matricula es 2021".$id."</td></tr>";
+      }
+    } //CUENTA NUMERO DE FILAS
+  
+    $tabla=$tabla."</table";
+    echo $tabla;
+  }else{
+    echo "Ya no es posible ingresar otro alumno";
   }
-
-  if ($res) {
-    $rowcount=mysqli_num_rows($res);
-    echo "The total number of rows are: ".$rowcount."</td></tr>";
-    if(intdiv($id, 10) == 0){
-      echo "Su matricula es 202100".$id."</td></tr>";
-    }if(intdiv($id, 10) < 10){
-      echo "Su matricula es 20210".$id."</td></tr>";
-    }else{
-      echo "Su matricula es 2021".$id."</td></tr>";
-    }
-  } //CUENTA NUMERO DE FILAS
-
-  $tabla=$tabla."</table";
-  echo $tabla;
-}else{
-  echo "Ya no es posible ingresar otro alumno";
-}
-mysqli_close($coneccion);
-
-
-?>
+  mysqli_close($coneccion);
+  
+  
+  ?>
--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<?php include_once("index.html");
-$host="us-cdbr-east-04.cleardb.com";
-$user="bba7ba2004ed75";
-$pass="8fcc9cd0";
-$db="heroku_6b4cf65f4c17d8e";
-$coneccion=mysqli_connect($host,$user,$pass,$db) or die("Error conección");
-$query="CREATE TABLE USER(id int AUTO_INCREMENT, nombre varchar(70) not null, fechanac date not null, PRIMARY KEY (id))";
-$res=mysqli_query($coneccion,$query) or die (" Error de consulta");
+<?php 
+// include_once("index.html");
+// $host="us-cdbr-east-04.cleardb.com";
+// $user="bba7ba2004ed75";
+// $pass="8fcc9cd0";
+// $db="heroku_6b4cf65f4c17d8e";
+// $coneccion=mysqli_connect($host,$user,$pass,$db) or die("Error conección");
+// $query="CREATE TABLE USER(id int AUTO_INCREMENT, nombre varchar(70) not null, fechanac date not null, PRIMARY KEY (id))";
+// $res=mysqli_query($coneccion,$query) or die (" Error de consulta");
+include_once("registro.html");
 ?>
